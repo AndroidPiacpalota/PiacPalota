a57dad5b5948aca87d63a047903d5f7229745f41
sz-kardos <sz.kardos@gmail.com>
2024-12-16 19:46:57 +0100

    register

--- app/src/main/java/com/example/piacpalota/u/i/HomeFragment.java
+++ app/src/main/java/com/example/piacpalota/u/i/HomeFragment.java
@@ -4,31 +4,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
 import com.example.piacpalota.R;
-import com.example.piacpalota.MainActivity;
 
 public class HomeFragment extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        Button loginButton = view.findViewById(R.id.welcBuyButton);
-        Button regButton = view.findViewById(R.id.welcSaleButton);
-
-        // Fragment tranzakció a helyes fragmenthez
-        loginButton.setOnClickListener(v -> {
-            // Használjuk a replaceFragment metódust a MainActivity-ben
-            ((MainActivity) requireActivity()).replaceFragment(new LogInFragment());
-        });
-
-        regButton.setOnClickListener(v -> {
-            // Ugyanez a regisztrációs fragmenthez
-            ((MainActivity) requireActivity()).replaceFragment(new SingInFragment());
+        // Regisztráció gomb eseménykezelése
+        TextView registButton = view.findViewById(R.id.registButton);
+        registButton.setOnClickListener(v -> {
+            NavController navController = Navigation.findNavController(view);
+            navController.navigate(R.id.registrationFragment);
         });
 
         return view;
---
+++ app/src/main/java/com/example/piacpalota/u/i/RegisterFragment.java
@@ -0,0 +1,21 @@
+package com.example.piacpalota.u.i;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.piacpalota.R;
+
+public class RegistrationFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_registration, container, false);
+    }
+}
